6d92611774ab7d94edfff81d77b314975645277b
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2024-03-21 13:32:40 +0530

    first commit

---
+++ 2.py
@@ -0,0 +1,10 @@
+my_list=[1,2,3]+[4,5],
+print(len(my_list))
+
+#comma at the end makes the entire list 
+#concatenated inside a my_list
+
+#Hence the tuple length becomes 1
+
+
+#if the comma is not there the length becomes 5
